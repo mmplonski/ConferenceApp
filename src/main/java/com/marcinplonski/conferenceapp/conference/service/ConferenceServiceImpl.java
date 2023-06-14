cf21a9a4089e4cc681097b63946f438a2fbb68dd
mmplonski <mplonski20@wp.pl>
2023-06-14 10:24:44 +0200

    Add to repository postman collection

--- src/main/java/com/marcinplonski/conferenceapp/conference/service/ConferenceServiceImpl.java
+++ src/main/java/com/marcinplonski/conferenceapp/conference/service/ConferenceServiceImpl.java
@@ -31,19 +31,19 @@ public class ConferenceServiceImpl implements ConferenceService {
                         "Przerwa na kawę: 11:45 - 12:00\n");
 
         prelectionService.getPrelectionsByStartTime(LocalTime.of(10, 00)).stream().forEach(prelection -> {
-            stringBuilder.append(prelection.getPath().toString() + ": " + prelection.getDescription() + "(" + prelection.getAvailableSeats() + "/5 wolnych miejsc)\n");
+            stringBuilder.append("[" + prelection.getId() + "]" + prelection.getPath().toString() + ": " + prelection.getDescription() + "(" + prelection.getAvailableSeats() + "/5 wolnych miejsc)\n");
         });
         stringBuilder.append("<pre>Czas trwania prelekcji: 12:00 - 13:45\n" +
                 "Przerwa na kawę: 11:45 - 12:00\n");
         prelectionService.getPrelectionsByStartTime(LocalTime.of(12, 00)).stream().forEach(prelection -> {
-            stringBuilder.append(prelection.getPath().toString() + ": " + prelection.getDescription() + "(" + prelection.getAvailableSeats() + "/5 wolnych miejsc)\n");
+            stringBuilder.append("[" + prelection.getId() + "]" + prelection.getPath().toString() + ": " + prelection.getDescription() + "(" + prelection.getAvailableSeats() + "/5 wolnych miejsc)\n");
         });
 
         stringBuilder.append("<pre>Czas trwania prelekcji: 14:00 - 15:45\n" +
                 "Przerwa na kawę: 11:45 - 12:00\n");
 
         prelectionService.getPrelectionsByStartTime(LocalTime.of(14, 00)).stream().forEach(prelection -> {
-            stringBuilder.append(prelection.getPath().toString() + ": " + prelection.getDescription() + "(" + prelection.getAvailableSeats() + "/5 wolnych miejsc)\n");
+            stringBuilder.append("[" + prelection.getId() + "]" + prelection.getPath().toString() + ": " + prelection.getDescription() + "(" + prelection.getAvailableSeats() + "/5 wolnych miejsc)\n");
         });
         stringBuilder.append("</pre>");
 
